c4813baf7dcce6c07f4112c43d0754f34aa1c538
jebax <joel.eliot.baxter@gmail.com>
2019-01-08 22:03:30 +0000

    AisleChart renders all correct react-vis components

--- client/src/__tests__/AisleChart.spec.js
+++ client/src/__tests__/AisleChart.spec.js
@@ -16,4 +16,13 @@ describe('Aisle data chart', () => {
   it('renders a react-vis chart', () => {
     expect(aisleChart.find('XYPlot').length).toEqual(1)
   })
+
+  it('displays time on the x-axis', () => {
+    const plot = aisleChart.find('XYPlot')
+    expect(plot.props().xType).toEqual('time')
+  })
+
+  it('renders two LineMarkSeries components', () => {
+    expect(aisleChart.find('LineMarkSeries').length).toEqual(2)
+  })
 })
--- client/src/components/AisleChart.js
+++ client/src/components/AisleChart.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { XYPlot } from 'react-vis'
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  LineMarkSeries
+} from 'react-vis'
 
 const AisleChart = () => {
   return (
     <XYPlot
       height={300}
       width={500}
+      xType={'time'}
     >
+      <HorizontalGridLines />
+      <VerticalGridLines />
+      <XAxis />
+      <YAxis />
+      <LineMarkSeries />
+      <LineMarkSeries />
     </XYPlot>
   )
 }
