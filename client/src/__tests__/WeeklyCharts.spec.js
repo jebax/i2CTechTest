b607d3dbd3e986d82cb2a35356da13cb1d6320f0
jebax <joel.eliot.baxter@gmail.com>
2019-01-08 21:58:04 +0000

    WeeklyCharts formats Aisle data and passes it to AisleChart

--- client/src/__tests__/WeeklyCharts.spec.js
+++ client/src/__tests__/WeeklyCharts.spec.js
@@ -1,27 +1,68 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import axios from 'axios'
 import WeeklyCharts from '../components/WeeklyCharts'
 
 jest.mock('axios')
 
-describe('Weekly data charts', () => {
+describe('Weekly charts wrapper', () => {
   var weeklyCharts
+  var weeklyData
 
   beforeAll(() => {
+    weeklyData = [
+      {
+        PRODUCT: 'Aisle',
+        WEEK_COMMENCING: '2018-01-01',
+        EXPOSED: '222',
+        CONTROL: '111'
+      },
+      {
+        PRODUCT: 'Brand',
+        WEEK_COMMENCING: '2018-01-01',
+        EXPOSED: '444',
+        CONTROL: '333'
+      },
+      {
+        PRODUCT: 'Offer',
+        WEEK_COMMENCING: '2018-01-01',
+        EXPOSED: '666',
+        CONTROL: '555'
+      }
+    ]
+
+    axios.get.mockResolvedValue({ data: weeklyData })
+
     weeklyCharts = shallow(<WeeklyCharts />)
   })
 
-  it('render correctly', () => {
+  it('renders correctly', () => {
     expect(weeklyCharts).toMatchSnapshot()
   })
 
-  it('include the AisleChart', () => {
+  it('makes the correct API request', () => {
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith(process.env.REACT_APP_WEEKLY_URL)
+  })
+
+  it('includes the AisleChart', () => {
     expect(weeklyCharts.find('AisleChart').length).toEqual(1)
   })
 
-  it('make the correct API request', () => {
-    expect(axios.get).toHaveBeenCalledTimes(1)
-    expect(axios.get).toHaveBeenCalledWith(process.env.REACT_APP_WEEKLY_URL)
+  it('passes the correct props to the AisleChart', () => {
+    const aisleChart = weeklyCharts.find('AisleChart')
+
+    const mappedData = [
+      {
+        x: new Date('2018-01-01'),
+        y: '222'
+      },
+      {
+        x: new Date('2018-01-01'),
+        y: '111'
+      }
+    ]
+
+    expect(aisleChart.props().data).toEqual(mappedData)
   })
 })
--- client/src/components/WeeklyCharts.js
+++ client/src/components/WeeklyCharts.js
@@ -4,14 +4,49 @@ import '../../node_modules/react-vis/dist/style.css'
 import AisleChart from './AisleChart'
 
 export default class WeeklyCharts extends Component {
+  state = {
+    aisleData: []
+  }
+
   componentDidMount() {
-    axios.get(process.env.REACT_APP_WEEKLY_URL)
+    axios.get(process.env.REACT_APP_WEEKLY_URL).then(response => {
+      this.formatData(response.data)
+    })
+  }
+
+  formatData = data => {
+    let aisleData = []
+
+    data.forEach(entry => {
+      if (entry.PRODUCT === 'Aisle') {
+        this.appendFormattedData(aisleData, entry)
+      }
+    })
+
+    this.setState(prevState => ({
+      aisleData: aisleData
+    }))
+  }
+
+  appendFormattedData = (dataCollection, entry) => {
+    dataCollection.push(
+      {
+        x: new Date(entry.WEEK_COMMENCING),
+        y: entry.EXPOSED
+      },
+      {
+        x: new Date(entry.WEEK_COMMENCING),
+        y: entry.CONTROL
+      }
+    )
   }
 
   render() {
     return(
       <div>
-        <AisleChart />
+        <AisleChart
+          data={this.state.aisleData}
+        />
       </div>
     )
   }
