a0636fe4ab4850c1e8a4f517dd6ec3a79d0f1cab
jebax <joel.eliot.baxter@gmail.com>
2019-01-08 17:00:50 +0000

    WeeklyChart makes request to API endpoint for weekly csv data

--- client/src/__tests__/weeklyChart.spec.js
+++ client/src/__tests__/weeklyChart.spec.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import axios from 'axios'
 import WeeklyChart from '../components/WeeklyChart'
+
+jest.mock('axios')
 
 describe('Weekly data chart', () => {
   var weeklyChart
@@ -11,5 +14,10 @@ describe('Weekly data chart', () => {
 
   it('renders correctly', () => {
     expect(weeklyChart).toMatchSnapshot()
+  })
+
+  it('makes the correct API request', () => {
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith(process.env.REACT_APP_WEEKLY_URL)
   })
 })
