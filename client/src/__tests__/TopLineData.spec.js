e9906053b75e9e61fd9e8244aeb9dc57b369d2b2
jebax <joel.eliot.baxter@gmail.com>
2019-01-09 17:29:43 +0000

    TopLineData passes correct data categories to TopLineChart

--- client/src/__tests__/TopLineData.spec.js
+++ client/src/__tests__/TopLineData.spec.js
@@ -10,10 +10,30 @@ describe('Top line data', () => {
   var fetchedData
 
   beforeAll(() => {
-    fetchedData = {
+    fetchedData = [
+      {
+        METRIC: 'Spend',
+        PRODUCT: 'Aisle',
+        PCT_UPLIFT: '0.01'
+      },
+      {
+        METRIC: 'Units',
+        PRODUCT: 'Aisle',
+        PCT_UPLIFT: '0.02'
+      },
+      {
+        METRIC: 'Visits',
+        PRODUCT: 'Aisle',
+        PCT_UPLIFT: '0.03'
+      },
+      {
+        METRIC: 'Total_custs',
+        PRODUCT: 'Aisle',
+        PCT_UPLIFT: '0.04'
+      }
+    ]
 
-    }
-    axios.get.mockResolvedValue(fetchedData)
+    axios.get.mockResolvedValue({ data: fetchedData })
 
     topLineData = shallow(<TopLineData />)
   })
@@ -29,5 +49,19 @@ describe('Top line data', () => {
 
   it('renders the TopLineChart', () => {
     expect(topLineData.find('TopLineChart').length).toEqual(1)
+  })
+
+  it('renders the TopLineChart with the correct data', () => {
+    const chart = topLineData.find('TopLineChart')
+
+    const spendData = [{ x: 'Aisle', y: '0.01' }]
+    const unitsData = [{ x: 'Aisle', y: '0.02' }]
+    const visitsData = [{ x: 'Aisle', y: '0.03' }]
+    const totalCustsData = [{ x: 'Aisle', y: '0.04' }]
+
+    expect(chart.props().spendData).toEqual(spendData)
+    expect(chart.props().unitsData).toEqual(unitsData)
+    expect(chart.props().visitsData).toEqual(visitsData)
+    expect(chart.props().totalCustsData).toEqual(totalCustsData)
   })
 })
--- client/src/components/TopLineData.js
+++ client/src/components/TopLineData.js
@@ -3,9 +3,56 @@ import axios from 'axios'
 import TopLineChart from './TopLineChart'
 
 export default class TopLineData extends Component {
+  state = {
+    spendData: [],
+    unitsData: [],
+    visitsData: [],
+    totalCustsData: []
+  }
+
   componentDidMount() {
     axios.get(process.env.REACT_APP_TOP_URL).then(response => {
-      console.log(response.data)
+      this.formatData(response.data)
+    })
+  }
+
+  formatData = data => {
+    let spendData = []
+    let unitsData = []
+    let visitsData = []
+    let totalCustsData = []
+
+    data.forEach(entry => {
+      switch (entry.METRIC) {
+        case 'Spend':
+          this.appendFormattedData(spendData, entry)
+          break
+        case 'Units':
+          this.appendFormattedData(unitsData, entry)
+          break
+        case 'Visits':
+          this.appendFormattedData(visitsData, entry)
+          break
+        case 'Total_custs':
+          this.appendFormattedData(totalCustsData, entry)
+          break
+        default:
+          break
+      }
+    })
+
+    this.setState(prevState => ({
+      spendData: spendData,
+      unitsData: unitsData,
+      visitsData: visitsData,
+      totalCustsData: totalCustsData
+    }))
+  }
+
+  appendFormattedData = (dataCollection, entry) => {
+    dataCollection.push({
+      x: entry.PRODUCT,
+      y: entry.PCT_UPLIFT
     })
   }
 
@@ -14,7 +61,12 @@ export default class TopLineData extends Component {
       <div
         className='topDataWrapper'
       >
-        <TopLineChart />
+        <TopLineChart
+          spendData={this.state.spendData}
+          unitsData={this.state.unitsData}
+          visitsData={this.state.visitsData}
+          totalCustsData={this.state.totalCustsData}
+        />
       </div>
     )
   }
