1af99655c8ff44d41b110cf706a5c06c247346e4
jebax <joel.eliot.baxter@gmail.com>
2019-01-09 13:33:59 +0000

    TopLineData makes API call for correct data

--- client/src/__tests__/TopLineData.spec.js
+++ client/src/__tests__/TopLineData.spec.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import axios from 'axios'
 import TopLineData from '../components/TopLineData'
+
+jest.mock('axios')
 
 describe('Top line data', () => {
   var topLineData
+  var fetchedData
 
   beforeAll(() => {
+    fetchedData = {
+
+    }
+    axios.get.mockResolvedValue(fetchedData)
+    
     topLineData = shallow(<TopLineData />)
   })
 
   it('renders correctly', () => {
     expect(topLineData).toMatchSnapshot()
+  })
+
+  it('makes the correct API request', () => {
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith(process.env.REACT_APP_TOP_URL)
   })
 })
--- client/src/components/TopLineData.js
+++ client/src/components/TopLineData.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class TopLineData extends Component {
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_TOP_URL).then(response => {
+      console.log(response.data)
+    })
+  }
+
   render() {
     return(
       <div
