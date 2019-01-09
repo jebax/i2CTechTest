f065278a5018075fea8ec4fdc999a0d6a65f4d1c
jebax <joel.eliot.baxter@gmail.com>
2019-01-09 17:42:31 +0000

    Update TopLineChart with x-axis time, prop types

--- client/src/__tests__/TopLineChart.spec.js
+++ client/src/__tests__/TopLineChart.spec.js
@@ -4,10 +4,24 @@ import TopLineChart from '../components/TopLineChart'
 
 describe('Top line chart', () => {
   var topLineChart
+  var spendData
+  var unitsData
+  var visitsData
+  var totalCustsData
 
   beforeAll(() => {
+    spendData = [{ x: 'Aisle', y: '0.01' }]
+    unitsData = [{ x: 'Aisle', y: '0.02' }]
+    visitsData = [{ x: 'Aisle', y: '0.03' }]
+    totalCustsData = [{ x: 'Aisle', y: '0.04' }]
+
     topLineChart = shallow(
-      <TopLineChart />
+      <TopLineChart
+        spendData={spendData}
+        unitsData={unitsData}
+        visitsData={visitsData}
+        totalCustsData={totalCustsData}
+      />
     )
   })
 
@@ -15,7 +29,12 @@ describe('Top line chart', () => {
     expect(topLineChart).toMatchSnapshot()
   })
 
-  it('renders an XYPlot', () => {
+  it('renders a react-vis chart', () => {
     expect(topLineChart.find('XYPlot').length).toEqual(1)
+  })
+
+  it('displays the time on its x-axis', () => {
+    const plot = topLineChart.find('XYPlot')
+    expect(plot.props().xType).toEqual('time')
   })
 })
--- client/src/components/TopLineChart.js
+++ client/src/components/TopLineChart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {
   XYPlot,
   XAxis,
@@ -7,11 +8,12 @@ import {
   HorizontalGridLines
 } from 'react-vis'
 
-const TopLineChart = () => {
+const TopLineChart = ({ spendData, unitsData, visitsData, totalCustsData }) => {
   return (
     <XYPlot
       height={300}
       width={600}
+      xType={'time'}
     >
     <XAxis />
     <YAxis />
@@ -19,6 +21,13 @@ const TopLineChart = () => {
     <HorizontalGridLines />
     </XYPlot>
   )
+}
+
+TopLineChart.propTypes = {
+  spendData: PropTypes.array.isRequired,
+  unitsData: PropTypes.array.isRequired,
+  visitsData: PropTypes.array.isRequired,
+  totalCustsData: PropTypes.array.isRequired
 }
 
 export default TopLineChart
