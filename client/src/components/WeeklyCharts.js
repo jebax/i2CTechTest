3bc941b94a213f4d680a3608b16f924c29729fe6
jebax <joel.eliot.baxter@gmail.com>
2019-01-09 14:07:35 +0000

    Refactored formatData function

--- client/src/components/WeeklyCharts.js
+++ client/src/components/WeeklyCharts.js
@@ -30,12 +30,18 @@ export default class WeeklyCharts extends Component {
     let offerControl = []
 
     data.forEach(entry => {
-      if (entry.PRODUCT === 'Aisle') {
-        this.appendFormattedData(aisleExposed, aisleControl, entry)
-      } else if (entry.PRODUCT === 'Brand') {
-        this.appendFormattedData(brandExposed, brandControl, entry)
-      } else {
-        this.appendFormattedData(offerExposed, offerControl, entry)
+      switch (entry.PRODUCT) {
+        case 'Aisle':
+          this.appendFormattedData(aisleExposed, aisleControl, entry)
+          break
+        case 'Brand':
+          this.appendFormattedData(brandExposed, brandControl, entry)
+          break
+        case 'Offer':
+          this.appendFormattedData(offerExposed, offerControl, entry)
+          break
+        default:
+          break
       }
     })
 
